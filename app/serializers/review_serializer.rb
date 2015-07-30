3be1912541ec90fc586a3000d13f1dd27d75cd23
Maksim <dollydasheep@gmail.com>
2015-07-30 11:13:29 -0400

    Fixed gemfile and switched off actualizers

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,3 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :created_at, :updated_at, :username, :total_reviews
+  # attributes :id, :created_at, :updated_at, :username, :total_reviews
 end
